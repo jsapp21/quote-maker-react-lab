b21529ec94b861f9fb177f07c5ba4b61fc931fee
James Sapp <jameseisapp@gmail.com>
2021-03-08 15:07:58 -0600

    commit tree ab47443f12aced53d99e5c12c0e05fa73d220d46

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import QuoteForm from './components/QuoteForm';
+import QuoteCard from './components/QuoteCard';
 
 class App extends Component {
   render() {
@@ -8,11 +10,13 @@ class App extends Component {
           <h1>Quote Maker</h1>
         </div>
         <hr />
-        {/*
-          TODO:
-
-          Render QuoteForm and Quotes Components
-         */}
+        {<QuoteForm />}
+        <hr />
+        <div className="row title justify-content-center" style={{ paddingTop: '12px' }}>
+          <h1>Quotes</h1>
+        </div>
+        <hr />
+        {<QuoteCard />}
       </div>
     );
   }
